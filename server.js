228961201c5170693903fa32d22c5550509b1c98
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-07 13:07:27 -0400

    done with stretch

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const CORS = require("cors");
 const projectsRouter = require("./projects/projectsRouter");
 const actionsRouter = require("./actions/actionsRouter");
 
 const server = express();
 
 server.use(express.json());
+server.use(CORS());
 
 server.use("/projects", projectsRouter);
 server.use("/actions", actionsRouter);
